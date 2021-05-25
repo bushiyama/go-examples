c11cd1eafcfe1fd2d06cb5f5392c163e48a08477
bushiyama <bushiyama.haru@gmail.com>
2021-05-25 22:05:02 +0900

    add: ox

---
+++ maru-batu/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+)
+
+const (
+	o = "o"
+	x = "x"
+)
+
+var flagvar string
+
+func main() {
+	flag.StringVar(&flagvar, "p", "", "[ox]{5}")
+	flag.Parse()
+
+	retMap := make(map[string]int, 3)
+	var drawFlg bool
+	for _, v := range strings.Split(flagvar, "") {
+		if v != o && v != x {
+			log.Fatal("o x only")
+		}
+		retMap[v]++
+		if retMap[v] == 3 {
+			fmt.Println(v + ": win!")
+			drawFlg = true
+			break
+		}
+	}
+	if !drawFlg {
+		fmt.Println("draw")
+	}
+}
